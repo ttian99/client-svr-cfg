ac81f4277f87487d5dacc2dfa618092b6bd0ae5b
ttian99 <176553611@qq.com>
2016-08-27 14:48:24 +0800

    修改zqy项目的端口为8810

--- zqy-comm-cfg.js
+++ zqy-comm-cfg.js
@@ -1,10 +1,11 @@
 function zqyCommCfg(cfg) {
   // 服务器地址
   if (cfg.isDev) {
-    cfg.svrUrl = 'http://localhost:8010/';
+    cfg.svrUrl = 'http://localhost:8810/';
   } else {
-    cfg.svrUrl = 'http://localhost:8010/';
+    cfg.svrUrl = 'http://120.24.209.148:8810/';
   }
+  cfg.port = 8810;
 };
 
 if (typeof(module) !== 'undefined') {
