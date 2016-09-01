67f930596952a0511d359c76d5a99d5f7c3489bf
ttian99 <176553611@qq.com>
2016-09-01 11:16:16 +0800

    修改各项目的对应配置

--- xwf-comm-cfg.js
+++ xwf-comm-cfg.js
@@ -1,10 +1,20 @@
 function xwfCommCfg(cfg) {
+  // 端口
+  cfg.port = 9001;
+  // 数据库类型
+  cfg.dbtype = 'postsql';
+  // 数据库名称
+  cfg.dbname ='jiaodb';
+
   // 服务器地址
   if (cfg.isDev) {
-    cfg.svrUrl = 'http://localhost:8010/';
+    cfg.svrUrl = 'http://localhost:'+ cfg.port + '/';
   } else {
-    cfg.svrUrl = 'http://localhost:8010/';
+    cfg.svrUrl = 'http://123.207.121.16:'+ cfg.port + '/';
   }
+
+
+  
 
   // 小区对应学校的数据
   // var svData = require('./xwf-sv-cfg.js');
--- zqy-comm-cfg.js
+++ zqy-comm-cfg.js
@@ -1,11 +1,17 @@
 function zqyCommCfg(cfg) {
+  // 端口
+  cfg.port = 9002;
+  // 数据库类型
+  cfg.dbtype = 'mongo';
+  // 数据库名称
+  cfg.dbname ='jiao';
   // 服务器地址
   if (cfg.isDev) {
-    cfg.svrUrl = 'http://localhost:8810/';
+    cfg.svrUrl = 'http://localhost:' + cfg.port + '/';
   } else {
-    cfg.svrUrl = 'http://120.24.209.148:8810/';
+    cfg.svrUrl = 'http://120.24.209.148:' + cfg.port + '/';
   }
-  cfg.port = 8810;
+
 };
 
 if (typeof(module) !== 'undefined') {
